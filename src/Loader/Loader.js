de4a25c7142679a8c28a559cf65d7f14ab3c7310
YuliyaBaskakova3012 <juliabaskakova3012@gmail.com>
2020-09-16 10:04:12 +0300

    1

---
+++ src/Loader/Loader.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import './Loader.css';
+export default()=><div className="loader-vertical-align"><div class="lds-spinner text-center"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
