a02284fbd05d639b3812b6595b4c7a746d6745f0
YuliyaBaskakova3012 <juliabaskakova3012@gmail.com>
2021-10-03 11:59:06 +0300

    03.10.2021 1

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,12 @@ import'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import {Provider} from "react-redux";
 import store from "./redux-store"
-//import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 ReactDOM.render(
-
+<HashRouter>
       <Provider store={store}>
           <App/>
-      </Provider>, document.getElementById('root'))
-
+      </Provider>
+</HashRouter>, document.getElementById('root'))
 
 
