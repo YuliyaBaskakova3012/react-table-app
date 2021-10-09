4d8f527f863bbe5ebe6e38d4797584ed4f115cc6
YuliyaBaskakova3012 <juliabaskakova3012@gmail.com>
2021-10-09 20:54:50 +0300

    09.10.2021 7

--- src/DataSelection/DataSelection.jsx
+++ src/DataSelection/DataSelection.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './DataSelection.css';
 export default props =>{
-const smallData='http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
-const bigData='http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+const smallData='https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+const bigData='https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
 return(
     <div className='vertical-align text-center'>
      <h1>Таблица с данными</h1> 
