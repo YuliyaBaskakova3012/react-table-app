1641b79d33179ac09195c2f0270109b6605c45b6
Your Name <you@example.com>
2022-04-18 22:21:07 +0300

    Updates

---
+++ precache-manifest.744895df5465c72c3b9bd682fd75d84c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08840bedfd01f9b762b886875df29fa9",
+    "url": "/react-table-app/index.html"
+  },
+  {
+    "revision": "ada2a0e08c2e643ac450",
+    "url": "/react-table-app/static/css/2.fc6a461e.chunk.css"
+  },
+  {
+    "revision": "b400cda01b11c61ef353",
+    "url": "/react-table-app/static/css/main.1a1cf188.chunk.css"
+  },
+  {
+    "revision": "ada2a0e08c2e643ac450",
+    "url": "/react-table-app/static/js/2.b39e2bd7.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/react-table-app/static/js/2.b39e2bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b400cda01b11c61ef353",
+    "url": "/react-table-app/static/js/main.ad706ede.chunk.js"
+  },
+  {
+    "revision": "8ad920e5c27221baa8c0",
+    "url": "/react-table-app/static/js/runtime-main.4938967e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-table-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-table-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-table-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-table-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-table-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
